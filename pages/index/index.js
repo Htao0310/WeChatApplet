05fa828831f66cb53bb9cc8aaf7359720053fc10
Hutao <hutao1993310@163.com>
2018-08-20 10:38:41 +0800

    修改主页设置

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,36 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '欢迎来到快捷盒子',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    id1: "postman",
+    id2: "cal",
+    id3: "weather",
+    id4: "making"
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+  /**
+   * 
+   */
+  clickView: function(e) {
+    console.log(e);
+    var btnValue = e.currentTarget.id;
+    console.log(btnValue);
+    if (btnValue == "postman") {
+      // 保留当前页面，跳转到应用内的某个页面，使用wx.navigateBack可以返回到原页面。
+      wx.navigateTo({
+        url: '../../pages/postman/postman'
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+    } else if (btnValue == "cal") {
+      wx.navigateTo({
+        url: '../../pages/cal/cal'
       })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    } else if (btnValue == "weather") {
+      wx.navigateTo({
+        url: '../../pages/weather/weather'
+      })
+    } else if (btnValue == "making") {
+      wx.navigateTo({
+        url: '../../pages/index/index'
+      })
+    } 
   }
 })
