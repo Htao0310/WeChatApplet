32c3e1899aa9f0f45a310a762d9b286d06033c66
Hutao <hutao1993310@163.com>
2018-08-20 16:14:37 +0800

    添加查询快递

--- pages/postman/postman.js
+++ pages/postman/postman.js
@@ -1,70 +1,113 @@
 // pages/postman/postman.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    result: '这里显示你的查询结果',
-  
+    expressNum: null,
+    // expressFirm: null,
+    expressInfo: null,
+
   },
-  btnClick: function (e){
-  
+  /**
+   * 获取文本数据
+   */
+  input: function(e) {
+    // console.log(e);
+    this.setData({
+      expressNum: e.detail.value
+    })
+  },
+
+  /**
+   * 点击查询按钮事件
+   */
+  btnClick: function() {
+    // 单号
+    var Num = this.data.expressNum;
+    var this2 = this;
+    // 调用查询快递公司的方法
+    app.getExpressFirm(Num, function(data) {
+      var Firm = data;
+      app.getExpressInfo(Firm, Num, function(data) {
+        console.log(data);
+        this2.setData({
+
+          expressInfo: data
+        })
+      })
+      this2.setData({
+        expressFirm: data
+      })
+    });
+    
+
+    // // 
+    // var Firm = this.data.expressFirm;
+    // // 调用查询快递信息
+    // app.getExpressInfo(Firm, Num, function(data) {
+    //   console.log(data);
+    //   this2.setData({
+    //     expressInfo: data
+    //   })
+    // })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  console.log("欢迎使用postman查询快递");
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
